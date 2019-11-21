9743c357682eecf4b0966f4038bc3983032f7b8e
AARON M <asmarcks@gmail.com>
2019-11-21 03:07:10 -0600

    working on  user validation and tokens

---
+++ dbschema.js
@@ -0,0 +1,13 @@
+let db = {
+    moans: [
+        
+            {
+                userHandle: 'user',
+                body: 'body body body',
+                createdAt: "2019-11-21T06:48:28.916Z",
+                likeCount: 5,
+                commentCount: 2
+            }
+        
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -5,89 +6,178 @@ const express = require('express');
 const app = express();
 const app2 = express();
 
+const config = {
+    apiKey: "AIzaSyDpre3LYFSaOES36poY1ikFNfGb1trJ8OI",
+    authDomain: "socialmediaproj1-d378f.firebaseapp.com",
+    databaseURL: "https://socialmediaproj1-d378f.firebaseio.com",
+    projectId: "socialmediaproj1-d378f",
+    storageBucket: "socialmediaproj1-d378f.appspot.com",
+    messagingSenderId: "658622002806",
+    appId: "1:658622002806:web:838514868dc0cdd5321157",
+    measurementId: "G-SKRLZ99Y9E"
+};
 
-        //
-/////Get requests//////////////////////////////////////////
-        //
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+//
+/////Get requests///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 
 
-app.get('/moans', (req, res)=>{
-    admin.firestore().collection('moans').get()
-    .then(data=>{
-        let moans = [];
-        data.forEach((doc)=>{
-            moans.push({
-                moanID: doc.id,
-                body: doc.data().body,
-                userHandle: doc.data().userHandle,
-                createdAt: doc.data().createdAt
-
-            });
-        });
-        return res.json(moans);
+app
+    .get('/moans', (req, res) => {
+        db
+            .collection('moans')
+            .orderBy('createdAt', 'desc')
+            .get()
+            .then(data => {
+                let moans = [];
+                data.forEach((doc) => {
+                    moans.push({
+                        moanID: doc.id,
+                        body: doc.data()
+                            .body,
+                        userHandle: doc.data()
+                            .userHandle,
+                        createdAt: doc.data()
+                            .createdAt
+                    })
+                })
+                return res.json(moans)
+            })
+            .catch(err => console.error(err))
     })
-    .catch(err=> console.error(err));
-})
-app2.get('/grunts', (req, res)=>{
-    admin.firestore().collection('grunts').get()
-    .then(data=>{
-        let grunts = [];
-        data.forEach((doc)=>{
-            grunts.push({
-                gruntID: doc.id,
-                body: doc.data().body,
-                userHandle: doc.data().userHandle,
-                createdAt: doc.data().createdAt
 
-            });
-        });
-        return res.json(grunts);
+app2
+    .get('/grunts', (req, res) => {
+        db
+            .collection('grunts')
+            .get()
+            .then(data => {
+                let grunts = []
+                data
+                    .forEach((doc) => {
+                        grunts
+                            .push({
+                                gruntID:
+                                    doc.id,
+                                body:
+                                    doc.data()
+                                        .body,
+                                userHandle:
+                                    doc.data()
+                                        .userHandle,
+                                createdAt:
+                                    doc.data()
+                                        .createdAt
+
+                            })
+                    })
+                return res.json(grunts)
+            })
+            .catch(err => console.error(err))
     })
-    .catch(err=> console.error(err));
-})
 
 
-        //
-/////Post requests//////////////////////////////////////////////////////
-        //
+//
+/////Post requests///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 
 
-app.post('/moan',(req, res)=>{
-    
-    const newMoan ={
-        body: req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
-    };
-    admin.firestore()
-        .collection('moans')
-        .add(newMoan)
-        .then(doc=>{
-            res.json({message: `document ${doc.id} created successfully`});
+app
+    .post('/moan', (req, res) => {
+
+        const newMoan = {
+            body: req.body.body,
+            userHandle: req.body.userHandle,
+            createdAt: new Date().toISOString()
+        };
+        db
+            .collection('moans')
+            .add(newMoan)
+            .then(doc => {
+                res
+                    .json({ message: `document ${doc.id} created successfully` })
+            })
+            .catch(err => {
+                res
+                    .status(500).json({ error: 'something went wrong' })
+                console.log(err)
+            })
+    });
+
+app2
+    .post('/grunt', (req, res) => {
+
+        const newGrunt = {
+            body: req.body.body,
+            userHandle: req.body.userHandle,
+            createdAt: new Date().toISOString()
+
+        };
+        db
+            .collection('grunts')
+            .add(newGrunt)
+            .then(doc => {
+                res
+                    .json({
+                        message:
+                            `document ${doc.id} created successfully`
+                    });
+            })
+            .catch(err => {
+                res
+                    .status(500).json({
+                        error:
+                            'something went wrong'
+                    });
+                console.log(err)
+            })
+    });
+
+
+//
+/////signUp route/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+
+    }
+
+    // TODO: validate data
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({ handle: 'this handle s already taken' })
+            } else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
         })
-        .catch(err=>{
-            res.status(500).json({error: 'something went wrong'});
-            console.log(err)
+        .then(data => {
+            return data.user.getIdToken()
         })
-});
-app2.post('/grunt',(req, res)=>{
-    
-    const newGrunt ={
-        body: req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
-    };
-    admin.firestore()
-    .collection('grunts')
-    .add(newGrunt)
-    .then(doc=>{
-        res.json({message: `document ${doc.id} created successfully`});
-    })
-    .catch(err=>{
-        res.status(500).json({error: 'something went wrong'});
-        console.log(err)
-    })
-});
+        .then(token => {
+            return res.status(201).json({ token })
+            // console.log(token)
+        })
+        .catch(err => {
+            console.error(err)
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json( 'try again retard. The email is takin.' )
+            } else {
+                return res.status(500).json({ error: err.code })
+            }
+        })
+})
 
-exports.api = functions.https.onRequest(app);
-exports.api2 = functions.https.onRequest(app2);
+
+exports.api = functions.https.onRequest(app)
+exports.api2 = functions.https.onRequest(app2)
